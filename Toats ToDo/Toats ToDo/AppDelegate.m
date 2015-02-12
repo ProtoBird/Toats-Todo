bf5b42ec6cf566bb0a4521b32355b3456bd92bc5
echo <echodeveloper@justecho.com>
2015-02-12 11:14:32 -0500

    Added Frameworks

--- Toats ToDo/Toats ToDo/AppDelegate.m
+++ Toats ToDo/Toats ToDo/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <Parse/Parse.h>
 
 @interface AppDelegate ()
 
@@ -16,7 +17,18 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    // [Optional] Power your app with Local Datastore. For more info, go to
+    // https://parse.com/docs/ios_guide#localdatastore/iOS
+    [Parse enableLocalDatastore];
+    
+    // Initialize Parse.
+    [Parse setApplicationId:@"QwEWdsePFjdgOQv2dH2PIuXGzfN8hesUNMcnE0Js"
+                  clientKey:@"eOT8HHp7Tk3x4v1nd18Dic6u4eieyZS3AXVJwYpV"];
+    
+    // [Optional] Track statistics around application opens.
+    [PFAnalytics trackAppOpenedWithLaunchOptions:launchOptions];
+    
+    // ...
     return YES;
 }
 
