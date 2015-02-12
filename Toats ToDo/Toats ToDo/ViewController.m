2576b149bccfb98e263c876a4f15615bd9c3c051
echo <echodeveloper@justecho.com>
2015-02-12 10:59:13 -0500

    Fresh Start with a Single page ViewController

---
+++ Toats ToDo/Toats ToDo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Toats ToDo
+//
+//  Created by Echo on 2/12/15.
+//  Copyright (c) 2015 Echo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
